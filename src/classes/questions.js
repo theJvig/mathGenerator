d69c64e2050c91137a2ae255d12410ee7f187848
Jacob Vig <jacobvig@gmail.com>
2021-12-16 21:15:19 -0500

    started to compile parts of equation to be able to later trigger 1 function to create new equation on submission

--- src/classes/questions.js
+++ src/classes/questions.js
@@ -22,12 +22,18 @@ class Questions extends React.Component{
         return operator[rand]; 
     }
 
+    randomEquation(){
+        let operator = ["+", "-", "/", "*"]; 
+        let randOperator = Math.floor(Math.random() * operator.length);
+    }
+
     render(){
         return(
             <div>
                 <p>Random Number 1: { this.state.randNumberOne } </p>
                 <p>Random Number 2: { this.state.randNumberTwo } </p>
                 <p>Random Operator: { this.state.randOperator } </p>
+                <p>Solve: {this.state.randNumberOne} {this.state.randOperator} {this.state.randNumberTwo}</p>
             </div>
             
         )
